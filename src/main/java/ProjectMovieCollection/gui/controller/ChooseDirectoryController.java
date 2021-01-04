276266be3efd747f691530cb68a8502b032f56b5
Kjell Schoke <kjell.gudrun-schoke@hotmail.com>
2021-01-04 15:02:46 +0100

    Update ChooseDirectoryController.java

--- src/main/java/ProjectMovieCollection/gui/controller/ChooseDirectoryController.java
+++ src/main/java/ProjectMovieCollection/gui/controller/ChooseDirectoryController.java
@@ -17,32 +17,37 @@ import javafx.stage.DirectoryChooser;
 
 public class ChooseDirectoryController {
 
-    @FXML
-    private VBox vBox;
-    @FXML
-    private TextField directoryTextField;
+    @FXML private VBox vBox;
+    @FXML private TextField directoryTextField;
 
     private final DirectoryChooser dirchoose = new DirectoryChooser();
     private final Directory dir = new Directory();
 
     public void browseButtonAction(ActionEvent actionEvent) {
+        // Opens the explorer and sets the selected directory as a file datatype.
         File file = dirchoose.showDialog(vBox.getScene().getWindow());
+
+        // Update the selected directory path.
         dir.setFilepath(file);
 
-        if (dir.getFilepath() != null) {
-            System.out.println(dir.getFilepath());
-            directoryTextField.setText(dir.getFilepath());
-        }
+        try {
+            if (dir.getFilepath() != null) {
+                // Sets the text field to the selected directory.
+                directoryTextField.setText(dir.getFilepath());
+            }
+        } catch (Exception ignored) {}
     }
 
     public void confirmButtonAction(ActionEvent actionEvent) {
         try {
             if (dir.getFilepath() != null) {
+                // Switch to the primary scene.
                 App.setRoot("view/primary", 1280, 720);
             }
         } catch (Exception e) {
+            // Open error prompt.
             Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Warning Dialog");
+            alert.setTitle("Error Dialog");
             alert.setHeaderText("No movie directory selected");
             alert.setContentText("Please choose a movie directory!");
             alert.showAndWait();
