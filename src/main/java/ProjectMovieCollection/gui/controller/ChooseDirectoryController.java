e4525be159684dfdeab39f90440d24ea3109557d
Kjell Schoke <kjell.gudrun-schoke@hotmail.com>
2021-01-04 14:46:04 +0100

    Update ChooseDirectoryController.java

--- src/main/java/ProjectMovieCollection/gui/controller/ChooseDirectoryController.java
+++ src/main/java/ProjectMovieCollection/gui/controller/ChooseDirectoryController.java
@@ -5,17 +5,15 @@
 package ProjectMovieCollection.gui.controller;
 
 import java.io.File;
-import java.io.IOException;
 
 import ProjectMovieCollection.App;
 import ProjectMovieCollection.bll.Directory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
-import javafx.stage.Stage;
 
 public class ChooseDirectoryController {
 
@@ -28,8 +26,7 @@ public class ChooseDirectoryController {
     private final Directory dir = new Directory();
 
     public void browseButtonAction(ActionEvent actionEvent) {
-        File file = dirchoose.showDialog((Stage) vBox.getScene().getWindow());
-
+        File file = dirchoose.showDialog(vBox.getScene().getWindow());
         dir.setFilepath(file);
 
         if (dir.getFilepath() != null) {
@@ -38,12 +35,17 @@ public class ChooseDirectoryController {
         }
     }
 
-    public void confirmButtonAction(ActionEvent actionEvent) throws IOException {
-        if (dir.getFilepath() != null) {
-            App.setRoot("view/primary",1280,720);
-        } else {
-            // TODO: Implement UI prompt
-            System.out.println("Choose a path");
+    public void confirmButtonAction(ActionEvent actionEvent) {
+        try {
+            if (dir.getFilepath() != null) {
+                App.setRoot("view/primary", 1280, 720);
+            }
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Warning Dialog");
+            alert.setHeaderText("No movie directory selected");
+            alert.setContentText("Please choose a movie directory!");
+            alert.showAndWait();
         }
     }
 }
