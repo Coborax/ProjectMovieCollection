396ee6b025604861d593b91b8d226e45f7fb2789
Mikkel Mouridsen <mikkelmouridsen@protonmail.com>
2021-01-04 14:40:17 +0100

    remove properties

--- src/main/java/ProjectMovieCollection/App.java
+++ src/main/java/ProjectMovieCollection/App.java
@@ -1,5 +1,6 @@
 package ProjectMovieCollection;
 
+import ProjectMovieCollection.bll.MovieData.MovieDBProvider;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
